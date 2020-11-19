ea2d551bedfd11100a04ca27d1508df08af98a08
Ciaran O'Reilly <ciaran@futureofvoice.com>
2020-11-19 22:49:02 +0100

    Working Vosk RecognitionService

---
+++ app/src/main/java/cat/oreilly/localstt/VoskRecognitionService.java
@@ -0,0 +1,178 @@
+package cat.oreilly.localstt;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.RemoteException;
+import android.speech.RecognitionService;
+import android.util.Log;
+
+import com.google.gson.Gson;
+import org.kaldi.Assets;
+import org.kaldi.KaldiRecognizer;
+import org.kaldi.Model;
+import org.kaldi.RecognitionListener;
+import org.kaldi.SpeechService;
+import org.kaldi.Vosk;
+
+import java.io.File;
+import java.util.Map;
+import java.util.ArrayList;
+import java.io.IOException;
+
+public class VoskRecognitionService extends RecognitionService implements RecognitionListener {
+    private final static String TAG = VoskRecognitionService.class.getName();
+    private KaldiRecognizer recognizer;
+    private SpeechService speechService;
+    private Model model;
+
+    private RecognitionService.Callback mCallback;
+
+    @Override
+    protected void onStartListening(Intent intent, Callback callback) {
+        mCallback = callback;
+        Log.i(TAG, "onStartListening");
+        runRecognizerSetup(intent);
+    }
+
+    @Override
+    protected void onCancel(Callback callback) {
+        Log.i(TAG, "onCancel");
+        results(new Bundle());
+    }
+
+    @Override
+    protected void onStopListening(Callback callback) {
+        Log.i(TAG, "onStopListening");
+        results(new Bundle());
+    }
+
+    private void runRecognizerSetup(final Intent intent) {
+        new AsyncTask<Void, Void, Exception>() {
+            @Override
+            protected Exception doInBackground(Void... params) {
+                try {
+                    Assets assets = new Assets(VoskRecognitionService.this);
+                    File assetDir = assets.syncAssets();
+                    Vosk.SetLogLevel(0);
+
+                    model = new Model(assetDir.toString() + "/vosk-catala");
+
+                    setupRecognizer();
+                } catch (IOException e) {
+                    return e;
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Exception result) {
+                if (result != null) {
+                    Log.e(TAG, "Failed to init recognizer " + result);
+                    error(android.speech.SpeechRecognizer.ERROR_CLIENT);
+                } else {
+                    readyForSpeech(new Bundle());
+                    beginningOfSpeech();
+
+                }
+            }
+        }.execute();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        if (speechService != null) {
+            speechService.cancel();
+            speechService.shutdown();
+        }
+    }
+
+    private void setupRecognizer() throws IOException {
+        try {
+            recognizer = new KaldiRecognizer(model, 16000.0f);
+            speechService = new SpeechService(recognizer, 16000.0f);
+            speechService.addListener(this);
+            speechService.startListening();
+        } catch (IOException e) {
+            Log.e(TAG, e.getMessage());
+        }
+    }
+
+    private void readyForSpeech(Bundle bundle) {
+        try {
+            mCallback.readyForSpeech(bundle);
+        } catch (RemoteException e) {
+            // empty
+        }
+    }
+
+    private void results(Bundle bundle) {
+        speechService.cancel();
+        try {
+            mCallback.results(bundle);
+        } catch (RemoteException e) {
+            // empty
+        }
+    }
+
+    private Bundle createResultsBundle(String hypothesis) {
+        ArrayList<String> hypotheses = new ArrayList<>();
+        hypotheses.add(hypothesis);
+        Bundle bundle = new Bundle();
+        bundle.putStringArrayList(android.speech.SpeechRecognizer.RESULTS_RECOGNITION, hypotheses);
+        return bundle;
+    }
+
+    private void beginningOfSpeech() {
+        try {
+            mCallback.beginningOfSpeech();
+        } catch (RemoteException e) {
+            // empty
+        }
+    }
+
+    private void error(int errorCode) {
+        speechService.cancel();
+        try {
+            mCallback.error(errorCode);
+        } catch (RemoteException e) {
+            // empty
+        }
+    }
+
+    @Override
+    public void onResult(String hypothesis) {
+        if (hypothesis != null) {
+            Log.i(TAG, hypothesis);
+            Gson gson = new Gson();
+            Map<String, String> map = gson.fromJson(hypothesis, Map.class);
+            String text = map.get("text");
+            results(createResultsBundle(text));
+        }
+    }
+
+    @Override
+    public void onPartialResult(String hypothesis) {
+        if (hypothesis != null) {
+            Log.i(TAG, hypothesis);
+            Gson gson = new Gson();
+            Map<String, String> map = gson.fromJson(hypothesis, Map.class);
+            String text = map.get("partial");
+            results(createResultsBundle(text));
+        }
+    }
+
+    @Override
+    public void onError(Exception e) {
+        Log.e(TAG, e.getMessage());
+        error(android.speech.SpeechRecognizer.ERROR_CLIENT);
+    }
+
+    @Override
+    public void onTimeout() {
+        speechService.cancel();
+        speechService.startListening();
+    }
+}
